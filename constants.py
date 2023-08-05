c4fb88955038730932fa04082c2e296b9d18163d
FimastGD <113923761+FimastGD@users.noreply.github.com>
2023-08-05 23:33:32 +0300

    Uploaded constants.py
    Config constants and other constants like a javascript

---
+++ constants.py
@@ -0,0 +1,18 @@
+# how to use
+# you can add variable for e.x. "VAR = 1" 
+# and load it in other py file:
+# import constants as const
+# print(const.VAR)
+# You can see result: "1"
+
+# config constants
+TOKEN = "YOURTOKEN" # replace on your token
+ERROR_EMOJI = "REPLACE ON YOUR <CROSS> EMOJI!"  # replace on your emoji
+
+# constants
+CRON1 = "`[LOG] Cron started`"
+CRON2 = "`[LOG] Cron started\n[LOG] Fixing language variables...`"
+CRON3 = "`[LOG] Cron started\n[LOG] Fixing language variables...\n[LOG] Language variables fixed!`"
+CRON4 = "`[LOG] Cron started\n[LOG] Fixing language variables...\n[LOG] Language variables fixed!\n[LOG] Fixing not-pinged users...\n[LOG] Fixed!`"
+CRON5 = "`[LOG] Cron started\n[LOG] Fixing language variables...\n[LOG] Language variables fixed!\n[LOG] Fixing not-pinged users...\n[LOG] Fixed!\n--------\nCRON ended!`"
+NOPERMCRON = "`[LOG] Cron started\n[LOG] Cron failure: no permissions`"
