5094198ae8df91950da2ae5bfd81680814e9ae59
FimastGD <113923761+FimastGD@users.noreply.github.com>
2023-08-05 23:27:43 +0300

    Uploaded web.py
    Bot WEB interface, you can remove it, remove in main.py "import web" and "web.start()"

---
+++ web.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request
+from threading import Thread
+import time
+import requests
+
+
+app = Flask('')
+
+@app.route('/')
+def home():
+  return "Deny bot is active!"
+
+def run():
+  app.run(host='0.0.0.0', port=80)
+
+def start():
+  t = Thread(target=run)
+  t.start()
